ca4d74a5edd44f18580b83367f2574832b9a33d0
Qi <qz2280@columbia.edu>
2018-03-23 01:10:06 -0400

    Big change
    change implementation

--- text_stream/tests/test_text_stream.py
+++ text_stream/tests/test_text_stream.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-# Created at Mar 7, 2018, by Qi Zhang
 """
 Test data is downloaded from
 `github <https://github.com/georgeyumnam/quantum-espresso-codes/blob/master/2-Al/1_SCF/Al.pw.in>_`.
 """
 
+import pathlib
 import unittest
-from io import StringIO
-from pathlib import PurePath
 
 from text_stream import *
 
@@ -18,14 +16,5 @@ class TestText(unittest.TestCase):
         with open(self.file) as f:
             self.text = f.read()
 
-    def test_type(self):
-        a = make_text_stream(self.file)
-        b = make_text_stream(self.text)
-        self.assertTrue(isinstance(a, TextFileStream))
-        self.assertTrue(isinstance(b, TextStream))
-        self.assertTrue(isinstance(a.infile_path, PurePath))
-        self.assertTrue(isinstance(b.stream, StringIO))
-
     def test_error(self):
-        self.assertRaises(FileNotFoundError, TextFileStream, 'data/file_not_exists')
-        self.assertRaises(TypeError, make_text_stream, 1)
+        self.assertRaises(FileNotFoundError, TextStream, pathlib.Path('data/file_not_exists'))
